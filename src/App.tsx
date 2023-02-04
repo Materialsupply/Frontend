f05bac990090ec528ca8afffde9b1a34e76779b1
Praveen Joseph <praveen26jose@gail.com>
2023-02-04 20:40:13 +0530

    Intial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const logo: string = require('./logo.svg').default
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <img src={logo} className='app-logo' alt='logo' />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
